374733824b192bef8c570110dd26b61b255be79d
Matt Gates <matthew.gates2001@gmail.com>
2022-08-31 15:59:05 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+
+
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.html"))
+})
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/styles.css"))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/client.js"))
+})
+
+
+
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, ()=> {
+    console.log(`Listening on port ${port}`)
+  })
+  
